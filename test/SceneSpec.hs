2e5438eea49fa7c0a31b0784f177b3415ffcee04
tkaaad97 <tkaaad97@gmail.com>
2019-04-09 23:28:21 +0900

    try to use osmesa

--- test/SceneSpec.hs
+++ test/SceneSpec.hs
@@ -1,9 +1,23 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
 module SceneSpec
     ( spec
     ) where
 
+import Foreign (Ptr, nullPtr)
+import qualified Graphics.GL as GL
 import Test.Hspec
 
+foreign import ccall "OSMesaCreateContextExt" ffiOSMesaCreateContextExt
+    :: GL.GLenum -> GL.GLint -> GL.GLint -> GL.GLint -> Ptr () -> IO (Ptr ())
+
+foreign import ccall "OSMesaMakeCurrent" ffiOSMesaMakeCurrent
+    :: Ptr () -> Ptr () -> GL.GLenum -> GL.GLsizei -> GL.GLsizei -> IO GL.GLboolean
+
 spec :: Spec
-spec = describe "addMesh" $ do
-    it "test" $ 1 `shouldBe` 2
+spec = describe "addMesh" $
+    runIO $ do
+        ctx <- ffiOSMesaCreateContextExt GL.GL_RGBA 16 0 0 nullPtr
+        if ctx == nullPtr
+            then putStrLn "failure"
+            else putStrLn "success"
+        putStrLn "hello"
