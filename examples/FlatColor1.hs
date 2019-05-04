88d17c77146cd1258b811bd9c2660e82c1ee3871
tkaaad97 <tkaaad97@gmail.com>
2019-05-04 18:08:04 +0900

    move function to example common part

--- examples/FlatColor1.hs
+++ examples/FlatColor1.hs
@@ -45,10 +45,7 @@ main = do
         let mesh = Mesh geometry material
         addMesh scene mesh
         camera <- newCamera proj la
-        control <- newSphericalControlDefault camera
-        setMouseButtonEventCallback w (enterSphericalControl control) (leaveSphericalControl control)
-        setEnterOrLeaveEventCallback w (const $ return ()) (leaveSphericalControl control)
-        setCursorMoveEventCallback w (updateSphericalControl control)
+        _ <- setCameraMouseControl w camera
         return (scene, camera)
 
     onDisplay (s, c) w = do
@@ -60,40 +57,3 @@ main = do
         render = do
             renderScene s c
             GLFW.swapBuffers w
-
-    setMouseButtonEventCallback w onPress onRelease =
-        let callback w' GLFW.MouseButton'1 GLFW.MouseButtonState'Pressed _ = go w' (onPress SphericalControlModeOrbit)
-            callback w' GLFW.MouseButton'2 GLFW.MouseButtonState'Pressed _ = go w' (onPress SphericalControlModeZoom)
-            callback w' GLFW.MouseButton'1 GLFW.MouseButtonState'Released _ = go w' onRelease
-            callback w' GLFW.MouseButton'2 GLFW.MouseButtonState'Released _ = go w' onRelease
-            callback _ _ _ _ = return ()
-            go w' f = do
-                (width, height) <- GLFW.getWindowSize w'
-                (x, y) <- GLFW.getCursorPos w'
-                f (calcControlPosition width height x y)
-        in GLFW.setMouseButtonCallback w (Just callback)
-
-    setEnterOrLeaveEventCallback w onEnter onLeave =
-        let callback w' GLFW.CursorState'InWindow    = go w' onEnter
-            callback w' GLFW.CursorState'NotInWindow = go w' onLeave
-            go w' f = do
-                (width, height) <- GLFW.getWindowSize w'
-                (x, y) <- GLFW.getCursorPos w'
-                f (calcControlPosition width height x y)
-        in GLFW.setCursorEnterCallback w (Just callback)
-
-    setCursorMoveEventCallback w onMove =
-        let callback w' x y = do
-                (width, height) <- GLFW.getWindowSize w'
-                onMove (calcControlPosition width height x y)
-        in GLFW.setCursorPosCallback w (Just callback)
-
-    calcControlPosition :: Int -> Int -> Double -> Double -> V2 Float
-    calcControlPosition width height x y =
-        let x' = if width > 0
-                then max 0 (min (fromIntegral width) (realToFrac x)) / fromIntegral width
-                else 0
-            y' = if height > 0
-                then max 0 (min (fromIntegral height) (realToFrac y)) / fromIntegral height
-                else 0
-        in V2 x' y'
--- examples/Sample.hs
+++ examples/Sample.hs
@@ -1,13 +1,17 @@
 module Sample
-    ( withWindow
+    ( setCameraMouseControl
     , shutdown
     , resizeWindow
+    , withWindow
     ) where
 
 import Control.Monad (forever, unless, when)
 import Control.Monad.IO.Class (liftIO)
 import qualified GLW
+import Graphics.Hree.Camera
+import Graphics.Hree.CameraControl.SphericalControl
 import qualified Graphics.UI.GLFW as GLFW
+import Linear (V2(..))
 import System.Exit (ExitCode(..), exitSuccess)
 
 withWindow :: Int -> Int -> String -> (GLFW.Window -> IO a) -> (a -> GLFW.Window -> IO ()) -> IO ()
@@ -44,3 +48,49 @@ shutdown win = do
 resizeWindow :: GLFW.WindowSizeCallback
 resizeWindow win w h =
     GLW.glViewport 0 0 (fromIntegral w) (fromIntegral h)
+
+setCameraMouseControl :: GLFW.Window -> Camera -> IO SphericalControl
+setCameraMouseControl w camera = do
+    control <- newSphericalControlDefault camera
+    setMouseButtonEventCallback w (enterSphericalControl control) (leaveSphericalControl control)
+    setEnterOrLeaveEventCallback w (const $ return ()) (leaveSphericalControl control)
+    setCursorMoveEventCallback w (updateSphericalControl control)
+    return control
+
+    where
+    setMouseButtonEventCallback w onPress onRelease =
+        let callback w' GLFW.MouseButton'1 GLFW.MouseButtonState'Pressed _ = go w' (onPress SphericalControlModeOrbit)
+            callback w' GLFW.MouseButton'2 GLFW.MouseButtonState'Pressed _ = go w' (onPress SphericalControlModeZoom)
+            callback w' GLFW.MouseButton'1 GLFW.MouseButtonState'Released _ = go w' onRelease
+            callback w' GLFW.MouseButton'2 GLFW.MouseButtonState'Released _ = go w' onRelease
+            callback _ _ _ _ = return ()
+            go w' f = do
+                (width, height) <- GLFW.getWindowSize w'
+                (x, y) <- GLFW.getCursorPos w'
+                f (calcControlPosition width height x y)
+        in GLFW.setMouseButtonCallback w (Just callback)
+
+    setEnterOrLeaveEventCallback w onEnter onLeave =
+        let callback w' GLFW.CursorState'InWindow    = go w' onEnter
+            callback w' GLFW.CursorState'NotInWindow = go w' onLeave
+            go w' f = do
+                (width, height) <- GLFW.getWindowSize w'
+                (x, y) <- GLFW.getCursorPos w'
+                f (calcControlPosition width height x y)
+        in GLFW.setCursorEnterCallback w (Just callback)
+
+    setCursorMoveEventCallback w onMove =
+        let callback w' x y = do
+                (width, height) <- GLFW.getWindowSize w'
+                onMove (calcControlPosition width height x y)
+        in GLFW.setCursorPosCallback w (Just callback)
+
+    calcControlPosition :: Int -> Int -> Double -> Double -> V2 Float
+    calcControlPosition width height x y =
+        let x' = if width > 0
+                then max 0 (min (fromIntegral width) (realToFrac x)) / fromIntegral width
+                else 0
+            y' = if height > 0
+                then max 0 (min (fromIntegral height) (realToFrac y)) / fromIntegral height
+                else 0
+        in V2 x' y'
