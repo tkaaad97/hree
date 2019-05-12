7a15d29141ac76065e860d240b89e1151f756568
tkaaad97 <tkaaad97@gmail.com>
2019-05-13 01:10:47 +0900

    add transformation type

--- src/Graphics/Hree/Math.hs
+++ src/Graphics/Hree/Math.hs
@@ -3,11 +3,17 @@ module Graphics.Hree.Math
     , Vec3
     , Vec4
     , ColorW8
+    , Transform(..)
+    , transformMatrix
     ) where
 
 import Data.Text (Text)
 import Data.Word (Word8)
-import qualified Linear as Linear (V2, V3, V4(..))
+import Foreign.Ptr (castPtr, plusPtr)
+import Foreign.Storable (Storable(..))
+import Linear ((^*))
+import qualified Linear (M44, Quaternion, V2, V3(..), V4, fromQuaternion,
+                         mkTransformation, mkTransformationMat)
 
 type Vec2 = Linear.V2 Float
 
@@ -16,3 +22,35 @@ type Vec3 = Linear.V3 Float
 type Vec4 = Linear.V4 Float
 
 type ColorW8 = Linear.V4 Word8
+
+type Quaternion = Linear.Quaternion Float
+
+type Mat4 = Linear.M44 Float
+
+data Transform = Transform
+    { transformTranslation :: !Vec3
+    , transformQuaternion  :: !Quaternion
+    , transformScale       :: !Vec3
+    } deriving (Show, Eq)
+
+instance Storable Transform where
+    sizeOf _ = 40
+
+    alignment _ = 4
+
+    peek ptr = do
+        t <- peek $ castPtr ptr
+        q <- peek $ castPtr ptr `plusPtr` 12
+        s <- peek $ castPtr ptr `plusPtr` 28
+        return $ Transform t q s
+
+    poke ptr (Transform t q s) = do
+        poke (castPtr ptr) t
+        poke (castPtr ptr `plusPtr` 12) q
+        poke (castPtr ptr `plusPtr` 28) s
+
+transformMatrix :: Transform -> Mat4
+transformMatrix (Transform t q (Linear.V3 sx sy sz)) =
+    let Linear.V3 v0 v1 v2 = Linear.fromQuaternion q
+        m = Linear.V3 (v0 ^* sx) (v1 ^* sy) (v2 ^* sz)
+    in Linear.mkTransformationMat m t
