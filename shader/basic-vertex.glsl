f1a52d994ac16d1adc8a620243d6b128be9a7cfb
tkaaad97 <tkaaad97@gmail.com>
2019-07-23 00:08:31 +0900

    skinning shader

--- shader/basic-vertex.glsl
+++ shader/basic-vertex.glsl
@@ -1,9 +1,12 @@
 #version 450
+#define MAX_BONES 256
 
 in vec3 position;
 in vec3 normal;
 in vec2 uv;
 in vec4 color;
+in vec4 boneIndex;
+in vec4 boneWeight;
 
 out vec3 fragmentNormal;
 out vec2 fragmentUv;
@@ -11,10 +14,17 @@ out vec4 fragmentColor;
 
 uniform mat4 projectionViewMatrix = mat4(1.0);
 uniform mat4 modelMatrix = mat4(1.0);
+uniform mat4 boneMatrices[ MAX_BONES ];
 
 void main()
 {
-    gl_Position = projectionViewMatrix * modelMatrix * vec4(position, 1.0);
+    mat4 boneMatrix =
+        boneWeight.x * boneMatrices[int(jointIndex.x)] +
+        boneWeight.y * boneMatrices[int(jointIndex.y)] +
+        boneWeight.z * boneMatrices[int(jointIndex.z)] +
+        boneWeight.w * boneMatrices[int(jointIndex.w)] +
+        clamp(1.0 - boneWeight.x - boneWeight.y - boneWeight.z - boneWeight.z, 0.0, 1.0) * mat4(1.0);
+    gl_Position = projectionViewMatrix * modelMatrix * boneMatrix * vec4(position, 1.0);
     fragmentNormal = normal;
     fragmentUv = uv;
     fragmentColor = color;
