d5b685768475546940d95ee2cddb7303575fb2d9
tkaaad97 <tkaaad97@gmail.com>
2020-01-26 15:22:47 +0900

    implementing tiled

--- src/Graphics/Format/Tiled.hs
+++ src/Graphics/Format/Tiled.hs
@@ -1,8 +1,13 @@
 module Graphics.Format.Tiled
     ( Rect(..)
+    , TiledConfig(..)
+    , createNodesFromTiledMap
+    , createNodesFromTiledMapWithConfig
+    , defaultTiledConfig
     , tileBoundingUpLeft
     , tileBoundingRect
     , tileBoundingSpriteVertex
+    , uvBoundingRect
     ) where
 
 import qualified Codec.Picture as Picture (Image(..), PixelRGBA8, convertRGBA8,
@@ -28,12 +33,14 @@ import qualified Graphics.Hree.Geometry as Hree (addVerticesToGeometry,
 import qualified Graphics.Hree.GL.Types as Hree (Texture(..))
 import qualified Graphics.Hree.GL.Vertex as Hree (SpriteVertex(..))
 import qualified Graphics.Hree.Material as Hree (spriteMaterial)
-import qualified Graphics.Hree.Scene as Hree (addMesh, addSampler, addTexture)
+import qualified Graphics.Hree.Scene as Hree (addMesh, addNode, addSampler,
+                                              addTexture, newNode)
 import qualified Graphics.Hree.Texture as Hree (TextureSettings(..),
                                                 TextureSourceData(..))
 import qualified Graphics.Hree.Types as Hree (Geometry, Material, Mesh(..),
-                                              MeshId, Scene)
+                                              MeshId, Node(..), NodeId, Scene)
 import Linear (V2(..), V3(..), (^-^))
+import Prelude hiding (map)
 import System.Directory (canonicalizePath)
 import System.FilePath ((</>))
 
@@ -54,27 +61,23 @@ data TiledConfig = TiledConfig
     { tiledConfigUnitLength  :: !Int
     , tiledConfigStartZ      :: !Float
     , tiledConfigLayerDeltaZ :: !Float
+    , tiledConfigOriginPixel :: !(V2 Int)
     } deriving (Show, Eq)
 
 defaultTiledConfig :: TiledConfig
-defaultTiledConfig = TiledConfig 1024 0 0
+defaultTiledConfig = TiledConfig 1024 0 0 (V2 0 0)
 
-{-
-createNodesFromTiled :: Tiled -> Scene -> IO ()
-createNodesFromTiled = undefined
--}
-
-tilesetGidRanges :: BV.Vector Tileset -> BV.Vector (Gid, Gid)
-tilesetGidRanges = BV.postscanl' go (0, 1)
+tilesetGidRanges :: BV.Vector Tileset -> BV.Vector (V2 Gid)
+tilesetGidRanges = BV.postscanl' go (V2 0 1)
     where
-    go (_, a) tileset =
+    go (V2 _ a) tileset =
         let fgid = maybe a (max a) (tilesetFirstGid tileset)
             next = fgid + fromIntegral (tilesetTileCount tileset)
-        in (fgid, next)
+        in V2 fgid next
 
-resolveTileset :: BV.Vector (Maybe TilesetInfo) -> BV.Vector Gid -> Gid -> Maybe TilesetInfo
-resolveTileset tilesets firstGids gid = do
-    index <- fmap (+ (-1)) . BV.findIndex (gid <) $ firstGids
+resolveTileset :: BV.Vector (Maybe TilesetInfo) -> BV.Vector (V2 Gid) -> Gid -> Maybe TilesetInfo
+resolveTileset tilesets gidRanges gid = do
+    index <- BV.findIndex (\(V2 firstGid nextGid) -> firstGid <= gid && gid < nextGid) $ gidRanges
     id =<< tilesets BV.!? index
 
 renderOrderIndex :: RenderOrder -> Int -> Int -> Int -> Int
@@ -82,54 +85,72 @@ renderOrderIndex RenderOrderRightDown _ _ i = i
 renderOrderIndex RenderOrderRightUp w h i =
     let (d, m) = divMod i w
     in w * (h - 1 - d) + m
-renderOrderIndex RenderOrderLeftDown w h i =
+renderOrderIndex RenderOrderLeftDown w _ i =
     let (d, m) = divMod i w
     in w * d + w - 1 - m
 renderOrderIndex RenderOrderLeftUp w h i =
     let (d, m) = divMod i w
     in w * (h - 1 - d) + w - 1 - m
 
-tileOriginPixelCoord :: Orientation -> V2 Int -> V2 Int -> V2 Int
-tileOriginPixelCoord = undefined
+createNodesFromTiledMap :: Hree.Scene -> FilePath -> Map -> IO (BV.Vector Hree.NodeId)
+createNodesFromTiledMap scene cd = createNodesFromTiledMapWithConfig scene cd defaultTiledConfig
 
-createMeshesFromTileLayer :: Hree.Scene -> TiledConfig -> Map -> BV.Vector (Maybe TilesetInfo) -> BV.Vector Gid -> TileLayer -> IO (BV.Vector Hree.MeshId)
-createMeshesFromTileLayer scene config map tilesetInfos firstGids tileLayer = BV.mapM (uncurry $ createMeshFromTiles scene config map layerData origin) groups
+createNodesFromTiledMapWithConfig :: Hree.Scene -> FilePath -> TiledConfig -> Map -> IO (BV.Vector Hree.NodeId)
+createNodesFromTiledMapWithConfig scene cd config map = do
+    tilesetInfos <- createTilesetInfos scene cd tilesets
+    BV.mapMaybe id <$> BV.imapM (createNodeFromLayer scene config map tilesetInfos gidRanges) layers
     where
-    go [] i gid = [(resolveTileset tilesetInfos firstGids gid, V2 i 1)]
-    go ((Nothing, V2 i0 n) : xs) _ gid = (resolveTileset tilesetInfos firstGids gid, V2 i0 (n + 1)) : xs
+    tilesets = mapTilesets map
+    gidRanges = tilesetGidRanges tilesets
+    layers = mapLayers map
+
+createNodeFromLayer :: Hree.Scene -> TiledConfig -> Map -> BV.Vector (Maybe TilesetInfo) -> BV.Vector (V2 Gid) -> Int -> Layer -> IO (Maybe Hree.NodeId)
+createNodeFromLayer scene config map tilesetInfos gidRanges layerIndex (LayerTileLayer layer) = do
+    meshIds <- createMeshesFromTileLayer scene config map tilesetInfos gidRanges layerIndex layer
+    nodeIds <- BV.mapM mkNode meshIds
+    Just <$> Hree.addNode scene Hree.newNode { Hree.nodeChildren = nodeIds } False
+    where
+    mkNode meshId = Hree.addNode scene Hree.newNode { Hree.nodeMesh = Just meshId } False
+createNodeFromLayer _ _ _ _ _ _ _ = return Nothing
+
+createMeshesFromTileLayer :: Hree.Scene -> TiledConfig -> Map -> BV.Vector (Maybe TilesetInfo) -> BV.Vector (V2 Gid) -> Int -> TileLayer -> IO (BV.Vector Hree.MeshId)
+createMeshesFromTileLayer scene config map tilesetInfos gidRanges layerIndex tileLayer = BV.mapM (uncurry $ createMeshFromTiles scene config map layerData origin z) groups
+    where
+    go [] i gid = [(resolveTileset tilesetInfos gidRanges gid, V2 i 1)]
+    go ((Nothing, V2 i0 n) : xs) _ gid = (resolveTileset tilesetInfos gidRanges gid, V2 i0 (n + 1)) : xs
     go a @ ((Just tileset, V2 i0 n) : xs) i gid =
-        case resolveTileset tilesetInfos firstGids gid of
+        case resolveTileset tilesetInfos gidRanges gid of
             Nothing -> (Just tileset, V2 i0 (n + 1)) : xs
             Just x | tilesetInfoIndex x == tilesetInfoIndex tileset -> (Just tileset, V2 i0 (n + 1)) : xs
                    | otherwise -> (Just x, V2 i 1) : a
     layerData = tileLayerData tileLayer
     width = mapWidth map
     height = mapHeight map
-    orientation = mapOrientation map
     renderOrder = mapRenderOrder map
     ordered = UV.generate (width * height) ((layerData UV.!) . renderOrderIndex renderOrder width height)
     groups = BV.reverse . BV.mapMaybe tilesetExists . BV.fromList . UV.ifoldl' go [] $ ordered
     tilesetExists (Just material, a) = Just (material, a)
     tilesetExists (Nothing, _)       = Nothing
-    origin = tileOriginPixelCoord orientation (V2 width height) (V2 (mapTileWidth map) (mapTileHeight map))
+    origin = tiledConfigOriginPixel config
+    z = tiledConfigStartZ config + tiledConfigLayerDeltaZ config * fromIntegral layerIndex
 
-createMeshFromTiles :: Hree.Scene -> TiledConfig -> Map -> UV.Vector Gid -> V2 Int -> TilesetInfo -> V2 Int -> IO Hree.MeshId
-createMeshFromTiles scene config map layerData origin tilesetInfo tiles = do
-    geometry <- createGeometryFromTiles scene config map layerData origin tilesetInfo tiles
+createMeshFromTiles :: Hree.Scene -> TiledConfig -> Map -> UV.Vector Gid -> V2 Int -> Float -> TilesetInfo -> V2 Int -> IO Hree.MeshId
+createMeshFromTiles scene config map layerData origin z tilesetInfo tiles = do
+    geometry <- createGeometryFromTiles scene config map layerData origin z tilesetInfo tiles
     let material = tilesetInfoMaterial tilesetInfo
         mesh = Hree.Mesh geometry material Nothing
     Hree.addMesh scene mesh
 
-createGeometryFromTiles :: Hree.Scene -> TiledConfig -> Map -> UV.Vector Gid -> V2 Int -> TilesetInfo -> V2 Int -> IO Hree.Geometry
-createGeometryFromTiles scene config map layerData origin tilesetInfo (V2 i0 n) = do
+createGeometryFromTiles :: Hree.Scene -> TiledConfig -> Map -> UV.Vector Gid -> V2 Int -> Float -> TilesetInfo -> V2 Int -> IO Hree.Geometry
+createGeometryFromTiles scene config map layerData origin z tilesetInfo (V2 i0 n) = do
     let vertices = SV.mapMaybe mkVertex $ SV.generate n (+ i0)
     (geo, _) <- Hree.newSpriteGeometry scene
     Hree.addVerticesToGeometry geo vertices GL.GL_STATIC_READ scene
     where
-    width = mapWidth map
-    height = mapHeight map
+    columns = mapWidth map
+    rows = mapHeight map
+    mapSize = V2 columns rows
     orientation = mapOrientation map
-    renderOrder = mapRenderOrder map
     mapTileSize = V2 (mapTileWidth map) (mapTileHeight map)
     tileset = tilesetInfoTileset tilesetInfo
     tilesetTileSize = V2 (tilesetTileWidth tileset) (tilesetTileHeight tileset)
@@ -140,10 +161,10 @@ createGeometryFromTiles scene config map layerData origin tilesetInfo (V2 i0 n) 
     unit = tiledConfigUnitLength config
     mkVertex i = do
         let gid = layerData UV.! i
-            (iy, ix) = divMod i width
+            (iy, ix) = divMod i columns
         uvRect <- uvBoundingRect tilesetInfo gid
-        let rect = tileBoundingRect orientation origin (V2 ix iy) mapTileSize tilesetTileSize offset unit staggerAxis staggerIndex hexSide
-            vertex = tileBoundingSpriteVertex rect 0 uvRect
+        let rect = tileBoundingRect orientation origin mapSize mapTileSize tilesetTileSize offset (V2 ix iy) unit staggerAxis staggerIndex hexSide
+            vertex = tileBoundingSpriteVertex rect z uvRect
         return vertex
 
 uvBoundingRect :: TilesetInfo -> Gid -> Maybe Rect
@@ -162,7 +183,6 @@ uvBoundingRect tilesetInfo gid =
     spacing = tilesetSpacing tileset
     tileWidth = tilesetTileWidth tileset
     tileHeight = tilesetTileHeight tileset
-    tsw = columns * tileWidth + (columns - 1) * spacing + margin * 2
     tsh = rows * tileHeight + (rows - 1) * spacing + margin * 2
     go lid =
         let (iy, ix) = divMod lid columns
@@ -174,9 +194,9 @@ uvBoundingRect tilesetInfo gid =
             h = fromIntegral tileHeight / fromIntegral uvHeight
         in Rect (V2 x y) (V2 w h)
 
-tileBoundingRect :: Orientation -> V2 Int -> V2 Int -> V2 Int -> V2 Int -> V2 Int -> Int -> StaggerAxis -> StaggerIndex -> Int -> Rect
-tileBoundingRect orientation origin index mapTileSize tilesetTileSize offset unit staggerAxis staggerIndex hexSide =
-    let upLeft = tileBoundingUpLeft orientation origin index mapTileSize offset unit staggerAxis staggerIndex hexSide
+tileBoundingRect :: Orientation -> V2 Int -> V2 Int -> V2 Int -> V2 Int -> V2 Int -> V2 Int -> Int -> StaggerAxis -> StaggerIndex -> Int -> Rect
+tileBoundingRect orientation origin mapSize mapTileSize tilesetTileSize offset index unit staggerAxis staggerIndex hexSide =
+    let upLeft = tileBoundingUpLeft orientation origin mapSize mapTileSize offset index unit staggerAxis staggerIndex hexSide
         V2 tileWidth tileHeight = tilesetTileSize
         w = fromIntegral tileWidth / fromIntegral unit
         h = fromIntegral tileHeight / fromIntegral unit
@@ -191,42 +211,56 @@ tileBoundingSpriteVertex (Rect (V2 x y) (V2 width height)) z (Rect uv uvSize) =
         vertex = Hree.SpriteVertex pos size angle uv uvSize
     in vertex
 
-tileBoundingUpLeft :: Orientation -> V2 Int -> V2 Int -> V2 Int -> V2 Int -> Int -> StaggerAxis -> StaggerIndex -> Int -> V2 Float
-tileBoundingUpLeft OrientationOrthogonal (V2 ox oy) (V2 ix iy) (V2 width height) (V2 offsetX offsetY) unit _ _ _ =
-    let x = fromIntegral (ox + ix * width + offsetX) / fromIntegral unit
-        y = fromIntegral (oy - iy * height - offsetY) / fromIntegral unit
+tileBoundingUpLeft :: Orientation -> V2 Int -> V2 Int -> V2 Int -> V2 Int -> V2 Int -> Int -> StaggerAxis -> StaggerIndex -> Int -> V2 Float
+tileBoundingUpLeft OrientationOrthogonal (V2 ox oy) (V2 _ rows) (V2 tileWidth tileHeight) (V2 offsetX offsetY) (V2 ix iy) unit _ _ _ =
+    let ox' = ox + 0
+        oy' = oy + rows * tileHeight
+        x = fromIntegral (ox' + ix * tileWidth + offsetX) / fromIntegral unit
+        y = fromIntegral (oy' - iy * tileHeight - offsetY) / fromIntegral unit
     in V2 x y
-tileBoundingUpLeft OrientationIsometric (V2 ox oy) (V2 ix iy) (V2 width height) (V2 offsetX offsetY) unit _ _ _ =
-    let x = fromIntegral (ox * 2 + (ix - iy) * width + offsetX * 2) / fromIntegral (unit * 2)
-        y = fromIntegral (oy * 2 - (ix + iy) * height - offsetY * 2) / fromIntegral (unit * 2)
+tileBoundingUpLeft OrientationIsometric (V2 ox oy) (V2 columns rows) (V2 tileWidth tileHeight) (V2 offsetX offsetY) (V2 ix iy) unit _ _ _ =
+    let halfw = (tileWidth + 1) `div` 2
+        halfh = (tileHeight + 1) `div` 2
+        ox' = ox + (rows - 1) * halfw
+        oy' = oy + (columns + rows) * halfh
+        x = fromIntegral (ox' + (ix - iy) * halfw + offsetX) / fromIntegral unit
+        y = fromIntegral (oy' - (ix + iy) * halfh - offsetY) / fromIntegral unit
     in V2 x y
-tileBoundingUpLeft OrientationStaggered (V2 ox oy) (V2 ix iy) (V2 width height) (V2 offsetX offsetY) unit staggerAxis staggerIndex _ =
-    let V2 sx sy = stagger2 staggerAxis staggerIndex 0 (V2 ix iy) (V2 width height)
-        x = fromIntegral (ox * 2 + sx + offsetX * 2) / fromIntegral (unit * 2)
-        y = fromIntegral (oy * 2 + sy - offsetY * 2) / fromIntegral (unit * 2)
+tileBoundingUpLeft OrientationStaggered (V2 ox oy) mapSize mapTileSize (V2 offsetX offsetY) ixy unit staggerAxis staggerIndex _ =
+    let V2 sx sy = stagger staggerAxis staggerIndex 0 mapSize mapTileSize ixy
+        x = fromIntegral (ox + sx + offsetX) / fromIntegral unit
+        y = fromIntegral (oy + sy - offsetY) / fromIntegral unit
     in V2 x y
-tileBoundingUpLeft OrientationHexagonal (V2 ox oy) (V2 ix iy) (V2 width height) (V2 offsetX offsetY) unit staggerAxis staggerIndex hexSide =
-    let V2 sx sy = stagger2 staggerAxis staggerIndex hexSide (V2 ix iy) (V2 width height)
-        x = fromIntegral (ox * 2 + sx + offsetX * 2) / fromIntegral (unit * 2)
-        y = fromIntegral (oy * 2 + sy - offsetY * 2) / fromIntegral (unit * 2)
+tileBoundingUpLeft OrientationHexagonal (V2 ox oy) mapSize mapTileSize (V2 offsetX offsetY) ixy unit staggerAxis staggerIndex hexSide =
+    let V2 sx sy = stagger staggerAxis staggerIndex hexSide mapSize mapTileSize ixy
+        x = fromIntegral (ox + sx + offsetX) / fromIntegral unit
+        y = fromIntegral (oy + sy - offsetY) / fromIntegral unit
     in V2 x y
 
-stagger2 :: StaggerAxis -> StaggerIndex -> Int -> V2 Int -> V2 Int -> V2 Int
-stagger2 StaggerAxisX staggerIndex hexSide (V2 ix iy) (V2 width height) =
-    let x = ix * (width + hexSide)
+stagger :: StaggerAxis -> StaggerIndex -> Int -> V2 Int -> V2 Int -> V2 Int -> V2 Int
+stagger StaggerAxisX staggerIndex hexSide (V2 _ rows) (V2 tileWidth tileHeight) (V2 ix iy)  =
+    let halfw = tileWidth - (tileWidth - hexSide) `div` 2
+        halfh = (tileHeight + 1) `div` 2
+        ox = 0
+        oy = rows * tileHeight + halfh
+        x = ox + ix * halfw
         y = case (staggerIndex, ix `mod` 2 == 0) of
-                (StaggerIndexEven, True)  -> - iy * height * 2
-                (StaggerIndexEven, False) -> - (iy + 1) * height * 2
-                (StaggerIndexOdd, False)  -> - iy * height * 2
-                (StaggerIndexOdd, True)   -> - (iy + 1) * height * 2
+                (StaggerIndexEven, True)  -> oy - (iy * tileHeight + halfh)
+                (StaggerIndexEven, False) -> oy - iy * tileHeight
+                (StaggerIndexOdd, False)  -> oy - (iy * tileHeight + halfh)
+                (StaggerIndexOdd, True)   -> oy - iy * tileHeight
     in V2 x y
-stagger2 StaggerAxisY staggerIndex hexSide (V2 ix iy) (V2 width height) =
-    let x = case (staggerIndex, iy `mod` 2 == 0) of
-                (StaggerIndexEven, True)  -> - ix * width * 2
-                (StaggerIndexEven, False) -> - (ix + 1) * width * 2
-                (StaggerIndexOdd, False)  -> - ix * width * 2
-                (StaggerIndexOdd, True)   -> - (ix + 1) * width * 2
-        y = - iy * (height + hexSide)
+stagger StaggerAxisY staggerIndex hexSide (V2 _ rows) (V2 tileWidth tileHeight) (V2 ix iy) =
+    let halfw = (tileWidth + 1) `div` 2
+        halfh = (tileHeight - (tileHeight - hexSide) `div` 2)
+        ox = 0
+        oy = (rows + 1) * halfh
+        x = case (staggerIndex, iy `mod` 2 == 0) of
+                (StaggerIndexEven, True)  -> ox + ix * tileWidth + halfw
+                (StaggerIndexEven, False) -> ox + ix * tileWidth
+                (StaggerIndexOdd, False)  -> ox + ix * tileWidth + halfw
+                (StaggerIndexOdd, True)   -> ox + ix * tileWidth
+        y = oy - iy * halfh
     in V2 x y
 
 createTilesetInfos :: Hree.Scene -> FilePath -> BV.Vector Tileset ->  IO (BV.Vector (Maybe TilesetInfo))
@@ -234,8 +268,8 @@ createTilesetInfos scene cd tilesets =
     let gidRanges = tilesetGidRanges tilesets
     in BV.imapM (createTilesetInfo scene cd) (BV.zip tilesets gidRanges)
 
-createTilesetInfo :: Hree.Scene -> FilePath -> Int -> (Tileset, (Gid, Gid)) -> IO (Maybe TilesetInfo)
-createTilesetInfo scene cd index (tileset, (firstGid, nextGid)) =
+createTilesetInfo :: Hree.Scene -> FilePath -> Int -> (Tileset, V2 Gid) -> IO (Maybe TilesetInfo)
+createTilesetInfo scene cd index (tileset, gidRange) =
     maybe (return Nothing) (fmap Just . go) (tilesetImage tileset)
     where
     go (Image sourcePath width height) = do
@@ -251,7 +285,7 @@ createTilesetInfo scene cd index (tileset, (firstGid, nextGid)) =
         let sname = Text.encodeUtf8 sourcePath
         (_, sampler) <- Hree.addSampler scene sname
         let material = Hree.spriteMaterial $ Hree.Texture (texture, sampler)
-        return $ TilesetInfo index tileset material (V2 twidth theight) (V2 firstGid nextGid)
+        return $ TilesetInfo index tileset material (V2 twidth theight) gidRange
     nextPow2 = nextPow2_ 1
     nextPow2_ a x | a >= x = a
                   | otherwise = nextPow2_ (a * 2) x
