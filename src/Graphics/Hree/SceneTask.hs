4456260b15da5ddd04fa2cfcd6cc218ee2cbad6d
tkaaad97 <tkaaad97@gmail.com>
2020-05-18 23:37:19 +0900

    record scene task last run date

--- src/Graphics/Hree/SceneTask.hs
+++ src/Graphics/Hree/SceneTask.hs
@@ -18,6 +18,7 @@ import Data.Atomics.Counter (AtomicCounter)
 import qualified Data.Atomics.Counter as Counter (incrCounter, newCounter)
 import Data.Hashable (Hashable)
 import qualified Data.HashTable.IO as HT
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
 import Graphics.Hree.Animation (AnimationClip(..), applyAnimationClip)
 import Graphics.Hree.Scene (removeNode)
 import Graphics.Hree.Types (NodeId, Scene)
@@ -50,50 +51,68 @@ data SceneTaskBoard = SceneTaskBoard
     { sceneTaskBoardScene   :: !Scene
     , sceneTaskBoardCounter :: !AtomicCounter
     , sceneTaskBoardItems   :: !(HashTable SceneTaskId SceneTask)
+    , sceneTaskBoardLastRun :: !(IORef (Maybe Time))
     }
 
 newSceneTaskBoard :: Scene -> IO SceneTaskBoard
 newSceneTaskBoard scene = do
     counter <- Counter.newCounter 1
     items <- HT.new
-    return $ SceneTaskBoard scene counter items
+    lastRunRef <- newIORef Nothing
+    return $ SceneTaskBoard scene counter items lastRunRef
 
 addSceneTask :: SceneTaskBoard -> SceneTask -> IO SceneTaskId
-addSceneTask (SceneTaskBoard _ counter items) task = do
+addSceneTask (SceneTaskBoard _ counter items _) task = do
     taskId <- SceneTaskId <$> Counter.incrCounter 1 counter
     HT.insert items taskId task
     return taskId
 
 removeSceneTask :: SceneTaskBoard -> SceneTaskId -> IO ()
-removeSceneTask (SceneTaskBoard _ _ items) taskId =
+removeSceneTask (SceneTaskBoard _ _ items _) taskId =
     HT.delete items taskId
 
 modifySceneTask :: SceneTaskBoard -> (SceneTask -> SceneTask) -> SceneTaskId -> IO ()
-modifySceneTask (SceneTaskBoard _ _ items) f taskId =
+modifySceneTask (SceneTaskBoard _ _ items _) f taskId =
     HT.mutate items taskId g
     where
     g a = (fmap f a, ())
 
-runSceneTask :: Scene -> SceneTask -> Time -> IO Bool
-runSceneTask _ Nop _ = return False
-runSceneTask scene (AnimationTask start animation options) t = do
+runEndAction :: Scene -> AnimationClip -> Maybe EndAction -> IO ()
+runEndAction _ _ Nothing = return ()
+runEndAction scene animation (Just SeekHead) = applyAnimationClip scene animation (Timespan 0)
+runEndAction _ _ (Just ClampEnd) = return ()
+runEndAction scene _ (Just (RemoveNode nodeId)) = removeNode scene nodeId
+
+runSceneTask :: Scene -> SceneTask -> Maybe Time -> Time -> IO Bool
+runSceneTask _ Nop _ _ = return False
+runSceneTask scene (AnimationTask start animation options) Nothing t = do
     let AnimationTaskOption loop removeAtEnd endAction = options
         duration = animationClipDuration animation
         dt = timeIntervalToTimespan $ TimeInterval start t
         remove = removeAtEnd && (duration >= dt)
-    when (t >= start) $ do
-        let at = if loop && dt > duration
-                    then Timespan $ getTimespan dt `mod` getTimespan duration
-                    else dt
-        applyAnimationClip scene animation at
-    when (not loop && dt >= duration) $ runEndAction endAction
+    when (t >= start) $
+        if loop
+            then applyAnimationClip scene animation . Timespan $ getTimespan dt `mod` getTimespan duration
+            else do
+                applyAnimationClip scene animation dt
+                when (dt >= duration) $ runEndAction scene animation endAction
     return remove
-    where
-    runEndAction Nothing = return ()
-    runEndAction (Just SeekHead) = applyAnimationClip scene animation (Timespan 0)
-    runEndAction (Just ClampEnd) = return ()
-    runEndAction (Just (RemoveNode nodeId)) = removeNode scene nodeId
-runSceneTask scene (RemoveNodeTask at nodeId) t =
+runSceneTask scene (AnimationTask start animation options) (Just lastRun) t
+    | lastRun == t = return False
+    | otherwise = do
+        let AnimationTaskOption loop removeAtEnd endAction = options
+            duration = animationClipDuration animation
+            dt = timeIntervalToTimespan $ TimeInterval start t
+            lastDt = timeIntervalToTimespan $ TimeInterval start lastRun
+            remove = removeAtEnd && (duration >= dt)
+        when (t >= start) $
+            if loop
+                then applyAnimationClip scene animation . Timespan $ getTimespan dt `mod` getTimespan duration
+                else do
+                    when (lastDt < duration) $ applyAnimationClip scene animation dt
+                    when (lastDt < duration && dt >= duration) $ runEndAction scene animation endAction
+        return remove
+runSceneTask scene (RemoveNodeTask at nodeId) _ t =
     if t >= at
         then do
             removeNode scene nodeId
@@ -101,12 +120,14 @@ runSceneTask scene (RemoveNodeTask at nodeId) t =
         else return False
 
 runSceneTasksOnBoard :: SceneTaskBoard -> Time -> IO ()
-runSceneTasksOnBoard (SceneTaskBoard scene _ items) t = do
-    removeTaskIds <- HT.foldM run [] items
+runSceneTasksOnBoard (SceneTaskBoard scene _ items lastRunRef) t = do
+    lastRun <- readIORef lastRunRef
+    removeTaskIds <- HT.foldM (run lastRun) [] items
     mapM_ (HT.delete items) removeTaskIds
+    writeIORef lastRunRef (Just t)
     where
-    run removeTaskIds (taskId, task) = do
-        remove <- runSceneTask scene task t
+    run lastRun removeTaskIds (taskId, task) = do
+        remove <- runSceneTask scene task lastRun t
         return $ if remove
                 then taskId : removeTaskIds
                 else removeTaskIds
