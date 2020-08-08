784d1066f4b9c7a213c015f16850bee8bacaebab
tkaaad97 <tkaaad97@gmail.com>
2020-08-08 19:14:14 +0900

    fix ci

--- scripts/build-draco.sh
+++ scripts/build-draco.sh
@@ -6,4 +6,7 @@ draco_dir="$1"
 build_dir="${draco_dir}/build"
 
 mkdir -p "${build_dir}"
-stack exec sh -- -c "cd ${build_dir} && cmake -DCMAKE_C_COMPILER=\$(which gcc) -DCMAKE_CXX_COMPILER=\$(which g++) -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_MAKE_PROGRAM=\$(which make) -DCMAKE_BUILD_TYPE=release .. && make"
+ccompiler=$(stack exec sh -- -c 'which gcc')
+cxxcompiler=$(stack exec sh -- -c 'which g++')
+makeprogram=$(stack exec sh -- -c 'which make')
+stack exec sh -- -c "cd ${build_dir} && cmake -DCMAKE_C_COMPILER=${ccompiler} -DCMAKE_CXX_COMPILER=${cxxcompiler} -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_MAKE_PROGRAM=${makeprogram} -DCMAKE_BUILD_TYPE=release .. && make"
