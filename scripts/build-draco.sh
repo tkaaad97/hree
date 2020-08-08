a6c619648f897be3aa1d542d2abdb81cbe8f112a
tkaaad97 <tkaaad97@gmail.com>
2020-08-08 17:18:43 +0900

    fix script and ci

--- scripts/build-draco.sh
+++ scripts/build-draco.sh
@@ -5,7 +5,5 @@ set -eu
 draco_dir="$1"
 build_dir="${draco_dir}/build"
 
-mkdir "$build_dir"
-cd "$build_dir"
-stack exec sh -- -c "cmake -DCMAKE_CXX_COMPILER=\$(which g++) -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_MAKE_PROGRAM=\$(which make) -DCMAKE_BUILD_TYPE=release .."
-stack exec make
+mkdir -p "${build_dir}"
+stack exec sh -- -c "cd ${build_dir} && cmake -DCMAKE_CXX_COMPILER=\$(which g++) -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_MAKE_PROGRAM=\$(which make) -DCMAKE_BUILD_TYPE=release .. && make"
