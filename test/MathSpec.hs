2fa89051afc869b661158b63ad08a55e50ccbbe0
tkaaad97 <tkaaad97@gmail.com>
2021-07-25 19:34:47 +0900

    try to fix test failure

--- test/MathSpec.hs
+++ test/MathSpec.hs
@@ -14,8 +14,8 @@ spec = do
         it "should decompose matrix to transform" . property $
             \((tx, ty, tz), (q0, q1, q2, q3), (sx, sy, sz)) -> do
                 let translate = V3 tx ty tz
-                    quaternion = normalize (Quaternion (correctNotZero q0) (V3 (correctNotZero q1) q2 q3))
-                    scale_ = V3 (abs $ correctNotZero sx) (abs $ correctNotZero sy) (abs $ correctNotZero sz)
+                    quaternion = normalize (Quaternion q0 (V3 (correctNotZero q1) q2 q3))
+                    scale_ = V3 (correctMaxAbs . abs . correctNotZero $ sx) (correctMaxAbs . abs . correctNotZero $ sy) (correctMaxAbs . abs . correctNotZero $ sz)
                     trans = Hree.Transform translate quaternion scale_
                     mat = Hree.transformMatrix trans
                     result = Hree.matrixToTransform mat
@@ -26,7 +26,10 @@ spec = do
                 ((trans, result), scale_ ^-^ scale') `shouldSatisfy` (nearZero . snd)
     where
     correctNotZero x
-        | x == 0 = 1.0E-4
-        | x > 0 = max 1.0E-4 x
-        | otherwise = - max 1.0E-4 (-x)
+        | x == 0 = 1.0E-2
+        | x > 0 = max 1.0E-2 x
+        | otherwise = - max 1.0E-2 (-x)
 
+    correctMaxAbs x
+        | abs x >= 100 = signum x * 100
+        | otherwise = x
