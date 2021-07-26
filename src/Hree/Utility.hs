370cbab6bca7ebf4cc4c31d238f42fb8c69ed24a
tkaaad97 <tkaaad97@gmail.com>
2021-07-26 21:08:13 +0900

    forgot to add

---
+++ src/Hree/Utility.hs
@@ -0,0 +1,17 @@
+module Hree.Utility
+    ( mesh
+    , node
+    , skinnedMesh
+    ) where
+
+import Hree.Types
+import qualified Linear (Quaternion(..), V3(..), V4, identity)
+
+mesh :: Geometry -> MaterialId b -> Mesh b
+mesh geometry materialId = Mesh geometry materialId Nothing Nothing Nothing
+
+skinnedMesh :: Geometry -> MaterialId b -> SkinId -> Mesh b
+skinnedMesh geometry materialId skinId_ = Mesh geometry materialId Nothing (Just skinId_) Nothing
+
+node :: Node
+node = Node Nothing mempty (Linear.V3 0 0 0) (Linear.Quaternion 1 (Linear.V3 0 0 0)) (Linear.V3 1 1 1) Linear.identity
